e5cc667cbe98e2e365c79e98e7ee86710e3b48c9
alvinmasaba <alvinmasaba@gmail.com>
2022-12-20 21:18:54 -0500

    Adds Friendship table to database.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
+  has_many :received_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :received_friends, through: :received_friendships, source: 'user'
+
+  def active_friends
+    friends.select { |friend| friend.friends.include?(self) }
+  end
+
+  def pending_friends
+    friends.select { |friend| !friend.friends.include?(self) }
+  end
 end
